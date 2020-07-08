cef88514c25d20a0cb98ff0a0c3c4dcdb012e175
T. Lanette <TLPollard4516@gmail.com>
2020-07-08 16:06:26 -0400

    Added js functions for getting info
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

---
+++ js/main.js
@@ -0,0 +1,249 @@
+const tag_active_class = 'tag-active';
+const search_hidden_class = 'search--hidden'
+const close_tag_class = 'close-tag';
+const tag_class = 'tag';
+
+// Job Listing information
+const jobData = [{
+        "id": 1,
+        "company": "Photosnap",
+        "logo": "./images/photosnap.svg",
+        "new": true,
+        "featured": true,
+        "position": "Senior Frontend Developer",
+        "role": "Frontend",
+        "level": "Senior",
+        "postedAt": "1d ago",
+        "contract": "Full Time",
+        "location": "USA Only",
+        "languages": ["HTML", "CSS", "JavaScript"],
+        "tools": []
+    },
+    {
+        "id": 2,
+        "company": "Manage",
+        "logo": "./images/manage.svg",
+        "new": true,
+        "featured": true,
+        "position": "Fullstack Developer",
+        "role": "Fullstack",
+        "level": "Midweight",
+        "postedAt": "1d ago",
+        "contract": "Part Time",
+        "location": "Remote",
+        "languages": ["Python"],
+        "tools": ["React"]
+    },
+    {
+        "id": 3,
+        "company": "Account",
+        "logo": "./images/account.svg",
+        "new": true,
+        "featured": false,
+        "position": "Junior Frontend Developer",
+        "role": "Frontend",
+        "level": "Junior",
+        "postedAt": "2d ago",
+        "contract": "Part Time",
+        "location": "USA Only",
+        "languages": ["JavaScript"],
+        "tools": ["React", "Sass"]
+    },
+    {
+        "id": 4,
+        "company": "MyHome",
+        "logo": "./images/myhome.svg",
+        "new": false,
+        "featured": false,
+        "position": "Junior Frontend Developer",
+        "role": "Frontend",
+        "level": "Junior",
+        "postedAt": "5d ago",
+        "contract": "Contract",
+        "location": "USA Only",
+        "languages": ["CSS", "JavaScript"],
+        "tools": []
+    },
+    {
+        "id": 5,
+        "company": "Loop Studios",
+        "logo": "./images/loop-studios.svg",
+        "new": false,
+        "featured": false,
+        "position": "Software Engineer",
+        "role": "FullStack",
+        "level": "Midweight",
+        "postedAt": "1w ago",
+        "contract": "Full Time",
+        "location": "Worldwide",
+        "languages": ["JavaScript"],
+        "tools": ["Ruby", "Sass"]
+    },
+    {
+        "id": 6,
+        "company": "FaceIt",
+        "logo": "./images/faceit.svg",
+        "new": false,
+        "featured": false,
+        "position": "Junior Backend Developer",
+        "role": "Backend",
+        "level": "Junior",
+        "postedAt": "2w ago",
+        "contract": "Full Time",
+        "location": "UK Only",
+        "languages": ["Ruby"],
+        "tools": ["RoR"]
+    },
+    {
+        "id": 7,
+        "company": "Shortly",
+        "logo": "./images/shortly.svg",
+        "new": false,
+        "featured": false,
+        "position": "Junior Developer",
+        "role": "Frontend",
+        "level": "Junior",
+        "postedAt": "2w ago",
+        "contract": "Full Time",
+        "location": "Worldwide",
+        "languages": ["HTML", "JavaScript"],
+        "tools": ["Sass"]
+    },
+    {
+        "id": 8,
+        "company": "Insure",
+        "logo": "./images/insure.svg",
+        "new": false,
+        "featured": false,
+        "position": "Junior Frontend Developer",
+        "role": "Frontend",
+        "level": "Junior",
+        "postedAt": "2w ago",
+        "contract": "Full Time",
+        "location": "USA Only",
+        "languages": ["JavaScript"],
+        "tools": ["Vue", "Sass"]
+    },
+    {
+        "id": 9,
+        "company": "Eyecam Co.",
+        "logo": "./images/eyecam-co.svg",
+        "new": false,
+        "featured": false,
+        "position": "Full Stack Engineer",
+        "role": "Fullstack",
+        "level": "Midweight",
+        "postedAt": "3w ago",
+        "contract": "Full Time",
+        "location": "Worldwide",
+        "languages": ["JavaScript", "Python"],
+        "tools": ["Django"]
+    },
+    {
+        "id": 10,
+        "company": "The Air Filter Company",
+        "logo": "./images/the-air-filter-company.svg",
+        "new": false,
+        "featured": false,
+        "position": "Front-end Dev",
+        "role": "Frontend",
+        "level": "Junior",
+        "postedAt": "1mo ago",
+        "contract": "Part Time",
+        "location": "Worldwide",
+        "languages": ["JavaScript"],
+        "tools": ["React", "Sass"]
+    }
+];
+
+//Function to get job tags
+
+function getTagHTML (tag, tagClasses) {
+    return `<span class="${tagClasses}">
+        ${tag}
+    </span>`;
+}
+
+// Function to get job information from jobData
+
+function getJobListingHTML(jobData, filterTags = []) {
+    const job_tags_placeholder = '###JOB_TAGS###';
+    let jobsListingHTML = `
+        <div class="jobs__item">
+            <div class="jobs__column jobs__column-left">
+                <img src="${jobData.logo}" alt = "${jobData.company}" class="jobs__img" />
+                <div class="jobs__info">
+                    <span class="jobs__company">${
+                    jobData.company} </span> 
+                    <span class="jobs__title">${
+                    jobData.position} </span>
+
+                    <ul class="jobs__details">
+                        <li class="jobs__details-item">${
+                        jobData.postedAt}</li> 
+                        <li class="jobs__details-item">${
+                        jobData.contract}</li> 
+                        <li class="jobs__details-item">${
+                        jobData.location}</li> 
+                    </ul> 
+                </div> 
+            </div> 
+            <div class="jobs__column jobs__column-right">
+            ${job_tags_placeholder}</div> 
+        </div>
+    `;
+
+    const tagsList = [
+        jobData.role,
+        jobData.level,
+        ...(jobData.languages || []),
+        ...(jobData.tools || [])
+    ];
+    const tagsListLowercase = tagsList.map(t => t && t.toLowerCase());
+    const passesFilter = !filterTags.length || filterTags.every(tag => (
+        tagsListLowercase.includes(tag && tag.toLowerCase())
+    ));
+
+    if (!passesFilter) {
+        return '';
+    }
+
+    const tagString = tagsList.reduce((acc, currentTag) => {
+        const activeClass = (filterTags.includes(currentTag) && tag_active_class) || '';
+
+        return acc + getTagHTML(currentTag, `${tag_class} ${activeClass}`);
+    }, '');
+
+    return jobsListingHTML.replace(job_tags_placeholder, tagString);
+};
+
+function toggleClass(el, className) {
+    if (el.classList.contains(className)) {
+        el.classList.remove(className);
+
+        return;
+    }
+
+    el.classList.add(className);
+}
+
+function getSearchBarTags(tagValue, searchContentEl) {
+    let searchBarTags = Array.from(searchContentEl.children).map(node => node.innerHTML && node.innerHTML.trim()).filter(tag => !!tag);
+
+    if (searchBarTags.includes(tagValue)) {
+        searchBarTags = searchBarTags.filter(tag => tag !== tagValue);
+    } else {
+        searchBarTags = [...searchBarTags, tagValue];
+    }
+
+    return searchBarTags;
+}
+
+function setJobListings(filterTags) {
+    const jobListingsHTML = jobListings.reduce((acc, currentListing) => {
+        return acc + getJobListingHTML(currentListing, filterTags);
+    }, '');
+
+    document.getElementById('jobs').innerHTML = jobsListingHTML;
+}
+
